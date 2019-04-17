93f0a05f031371048d4ff1b4d09888c98a4effe0
heragarden <49705364+heragarden@users.noreply.github.com>
2019-04-17 12:47:01 +0900

    Add files via upload

---
+++ java02/src/cal/CalTest.java
@@ -0,0 +1,39 @@
+package cal;
+
+public class CalTest {
+
+	public static void main(String[] args) {
+//		1.
+//		200과 100을 더해서,
+//		300이 넘으면,
+//			300에서 100을 빼서 프린트
+//		300이 넘지 않으면,
+//			300에 0.1을 곱해서 프린트
+		
+		int num1 = 100;
+		int num2 = 100;
+		int result = num1 + num2;
+		
+		if(result >= 300) {
+			System.out.println(result - 100);
+		}else {
+			System.out.println(result * 0.1);
+		}
+		
+		
+		
+//		2.
+//		정수 400이 짝수인지 판별하세요.
+//		(2로 나누어서 나머지가 0이면 짝수,나머지 연산자)
+		int num3 = 400;
+		int result2 = num3 % 2; 
+		if(result2 == 0) {
+			System.out.println("짝수");
+		}else {
+			System.out.println("홀수");
+		}
+		
+		
+	}
+
+}
